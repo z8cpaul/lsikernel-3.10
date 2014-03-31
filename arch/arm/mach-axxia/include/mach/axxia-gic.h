02007fdcf1f1a5125ff76964a586d632f9e926a4
Anders Berg <anders.berg@lsi.com>
2014-03-31 21:05:16 +0200

    ARM: axxia: Alloc GIC to be initialized from non-primary core
    The condition to only initialize the distributor interface from the primary
    core of each cluster could fail when the primary core is not started (e.g. in a
    crash kernel started via kexec). In this case, the one and only CPU may be any
    core in a cluster.
    
    This change handles this by initializing the GIC distributor interface via the
    first CPU calling the init function, by keeping a state which clusters have
    been initialized.
    
    Signed-off-by: Anders Berg <anders.berg@lsi.com>

--- arch/arm/mach-axxia/include/mach/axxia-gic.h
+++ arch/arm/mach-axxia/include/mach/axxia-gic.h
@@ -9,7 +9,6 @@
 #define __AXXIA_GIC_H
 
 void axxia_gic_raise_softirq(const struct cpumask *mask, unsigned int irq);
-void axxia_gic_secondary_cluster_init(void);
 void axxia_gic_secondary_init(void);
 int __init axxia_gic_of_init(struct device_node *node,
 			     struct device_node *parent);
